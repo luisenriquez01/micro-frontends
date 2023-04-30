c661d358ccd216a93fe8fdc3be6bc7db05d6df5c
Luis Carlos Enríquez <luis.enriquez01@telusinternational.com>
2023-04-30 12:56:36 -0600

    fix: class name collisions

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
 import MarketingLandingPage from "./components/MarketingLandingPage";
 import Header from "./components/Header";
+
+const genetateClassName = createGenerateClassName({
+	productionPrefix: "co",
+});
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Header />
-			<MarketingLandingPage />
+			<StylesProvider generateClassName={genetateClassName}>
+				<Header />
+				<MarketingLandingPage />
+			</StylesProvider>
 		</BrowserRouter>
 	);
 }
