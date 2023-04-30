89122310bd46a049a008a6d29198329dd5cd6c16
Luis Carlos Enríquez <luis.enriquez01@telusinternational.com>
2023-04-30 07:16:01 -0600

    container: add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingLandingPage from "./components/MarketingLandingPage";
+import Header from "./components/Header";
 
 export default function App() {
 	return (
-		<>
+		<BrowserRouter>
+			<Header />
 			<MarketingLandingPage />
-		</>
+		</BrowserRouter>
 	);
 }
